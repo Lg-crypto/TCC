a72327f2f1f513f59394fabab00c323ccabac50b
Lg-crypto <halejandroalvesoliveira10@gmail.com>
2026-05-25 19:50:44 -0300

    I added and configured a routing system using react-router-dom, removed some files, and edited what was necessary.

---
+++ Front/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import styles from "./home.module.css";
+
+export default function Home() {
+    return (
+        <>
+            <h2 className={styles.h2}>Inicio</h2>
+        </>
+    );
+}
---
+++ Front/src/pages/welcome.tsx
@@ -0,0 +1,7 @@
+export default function Welcome(){
+    return (
+        <>
+            <h2>This is Elon Musk</h2>
+        </>
+    );
+}
