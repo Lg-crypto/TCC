10386d1afe86213f74fe25b9e2b722788de8340c
Lg-crypto <halejandroalvesoliveira10@gmail.com>
2026-06-24 20:43:09 -0300

    Implement login and register forms with validation; add reusable Input and ModalMessage components; update styles and dependencies

--- Front/src/pages/login.tsx
+++ Front/src/pages/login.tsx
@@ -1,27 +1,85 @@
 import styles from "./login.module.css";
 import SideMenu from "../components/layout/sideMenu";
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import Button from "../components/layout/button"
+import Input from "../components/layout/input";
+import { useForm } from "react-hook-form";
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import ModalMessage from "../components/layout/modalMessage";
+
+import { type UserType } from "../types/userType";
+import { useState } from "react";
+
+type FormValues = {
+    email: string,
+    password: string
+}
+
+
+const loginSchema = z.object({
+
+    email: z.email({message: 'Informe um e-mail válido.'}),
+
+    password: z.string()
+            .min(6, {message: 'Informe uma senha com 6 caracteres ou mais.'})
+})
 
 export default function Login(){
+    
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false)
+
+    const { 
+        register, handleSubmit, formState:{errors} 
+    } = useForm<FormValues>(
+        {resolver: zodResolver(loginSchema)}
+    )
+
+    const [userData, setUserData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const userAuthenticate = (data: FormValues)=>{
+        setUserData({
+            name: '',
+            email: data.email,
+            password: data.password
+        });
+        setOpen(true);
+        console.log(userData);
+        //navigate('/home')
+    }
+
     return (
         <div className={styles.container}>
             <SideMenu></SideMenu>
             <div className={styles.content}>
-                <form className={styles.form}>
+                <form 
+                    className={styles.form}
+                    onSubmit={handleSubmit(userAuthenticate)}    
+                >
                     <div className={styles.logo}></div>
 
-                    <div className={styles.inputGroup}>
-                        <label htmlFor="email">Email</label>
-                        <input type="text" id="email" className={styles.input}/>
-                    </div>
-                    <div className={styles.inputGroup}>
-                        <label htmlFor="password">Password</label>
-                        <input type="text" id="password" className={styles.input}/>
-                    </div>
+                    <Input type="email" inputName="E-mail" isRequired={true} register={register('email')}></Input>
+                    <Input type="password" inputName="Password" isRequired={true} register={register('password')}></Input>
+                    { errors.email && <p>{errors.email.message}</p> }
+                    { errors.password && <p>{errors.password.message}</p> }
 
-                    <Button width={150} height={50} type="submit" className="sendButton">Entrar</Button>
-                    <Link to="/register">Ainda não tem uma conta?</Link>
+                    <Button width={150} height={50}>Entrar</Button>
+                    <ModalMessage
+                        isOpen={open}
+                        title="Aviso"
+                        onClose={() => setOpen(false)}
+                    >
+                        
+                        <p>Nome: {userData.name}</p>
+                        <p>E-mail: {userData.email}</p>
+                        <p>Senha: {userData.password}</p>
+                    </ModalMessage>
+                    <Link to="/register" className={styles.link}>Ainda não tem uma conta?</Link>
                 </form>
             </div>
         </div>
--- Front/src/pages/register.tsx
+++ Front/src/pages/register.tsx
@@ -1,10 +1,90 @@
+import styles from "./login.module.css";
 import SideMenu from "../components/layout/sideMenu";
+import {Link, useNavigate} from "react-router-dom"
+import Button from "../components/layout/button"
+import Input from "../components/layout/input";
+import { useForm } from "react-hook-form";
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import ModalMessage from "../components/layout/modalMessage";
+
+import { type UserType } from "../types/userType";
+import { useState } from "react";
+
+type FormValues = {
+    name: string
+    email: string,
+    password: string
+}
+
+
+const loginSchema = z.object({
+
+    email: z.email({message: 'Informe um e-mail válido.'}),
+    name: z.string()
+            .min(6, {message: 'Informe um nome com 6 caracteres ou mais.'}),
+
+    password: z.string()
+            .min(6, {message: 'Informe uma senha com 6 caracteres ou mais.'})
+})
 
 export default function Register(){
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false)
+
+    const { 
+        register, handleSubmit, formState:{errors} 
+    } = useForm<FormValues>(
+        {resolver: zodResolver(loginSchema)}
+    )
+
+    const [userData, setUserData]= useState<UserType>({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const userAuthenticate = (data: FormValues)=>{
+        setUserData({
+            name: '',
+            email: data.email,
+            password: data.password
+        });
+        setOpen(false)
+        console.log(userData);
+        //navigate('/home')
+    }
+
     return (
-        <>
+        <div className={styles.container}>
             <SideMenu></SideMenu>
-            <h2>Register</h2>
-        </>
+            <div className={styles.content}>
+                <form 
+                    className={styles.form}
+                    onSubmit={handleSubmit(userAuthenticate)}    
+                >
+                    <div className={styles.logo}></div>
+
+                    <Input type="email" inputName="E-mail" isRequired={true} register={register('email')}></Input>
+                    <Input type="text" inputName="Name" isRequired={true} register={register('name')}></Input>
+                    <Input type="password" inputName="Password" isRequired={true} register={register('password')}></Input>
+                    { errors.email && <p>{errors.email.message}</p> }
+                    { errors.password && <p>{errors.password.message}</p> }
+
+                    <Button width={150} height={50}>Entrar</Button>
+                    <ModalMessage
+                                            isOpen={open}
+                                            title="Aviso"
+                                            onClose={() => setOpen(false)}
+                                        >
+                                            
+                                            <p>Nome: {userData.name}</p>
+                                            <p>E-mail: {userData.email}</p>
+                                            <p>Senha: {userData.password}</p>
+                                        </ModalMessage>
+                    <Link to="/register" className={styles.link}>Ainda não tem uma conta?</Link>
+                </form>
+            </div>
+        </div>
     );
 }
